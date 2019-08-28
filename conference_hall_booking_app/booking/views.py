ac34da4fd4c9c9f522888417e14a86c11deaaee4
halon <halon.filip@gmail.com>
2019-08-28 22:57:02 +0200

    Work on booking in progress

--- conference_hall_booking_app/booking/views.py
+++ conference_hall_booking_app/booking/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render, redirect
 from django.views import View
 from django.http import HttpResponse
+# from django.utils.decorators import method_decorator
+# from django.views.decorators.csrf import csrf_exempt
 from .models import ConferenceHall, Booking
+from datetime import date, timedelta
 
 # Create your views here.
+
+
+def show_all_halls(request):
+    halls = ConferenceHall.objects.all()
+    return render(request, 'show_all_halls.html', {'halls': halls})
+
+
+def show_hall_details(request, hall_id):
+    hall = ConferenceHall.objects.get(pk=hall_id)
+    return render(request, 'show_hall_details.html', {'hall': hall})
 
 
 class AddNewHall(View):
@@ -17,13 +30,13 @@ class AddNewHall(View):
         try:
             capacity = int(request.POST.get('capacity'))
         except ValueError:
-            return HttpResponse("Pojemność nie jest liczbą całkowitą.")
+            return HttpResponse("Capacity is not an integer.")
         projector_available = request.POST.get('projector_available')
 
         ConferenceHall.objects.create(name=name,
                                       capacity=capacity,
                                       projector_available=projector_available)
-        return redirect('add-new-hall')
+        return redirect('home')
 
 
 class ModifyHall(View):
@@ -38,11 +51,11 @@ class ModifyHall(View):
         try:
             hall.capacity = request.POST['capacity']
         except ValueError:
-            return HttpResponse("Pojemność nie jest liczbą całkowitą.")
+            return HttpResponse("Capacity is not a integer.")
         hall.projector_available = request.POST['projector_available']
         hall.save()
 
-        return HttpResponse("Zmieniono")
+        return redirect('home')
 
 
 class DeleteHall(View):
@@ -54,14 +67,41 @@ class DeleteHall(View):
     def post(self, request, hall_id):
         hall = ConferenceHall.objects.get(pk=hall_id)
         hall.delete()
-        return HttpResponse("Usunięto")
+        return redirect('home')
 
 
-def show_hall_details(request, hall_id):
-    hall = ConferenceHall.objects.get(pk=hall_id)
-    return render(request, 'show_hall_details.html', {'hall': hall})
+class MakeReservation(View):
+
+    def get(self, request, hall_id):
+
+        hall = ConferenceHall.objects.get(pk=hall_id)
+
+        today = date.today()
+        date_range = int((date(2020, 1, 1) - today).days)
+        dates = [today + timedelta(day) for day in range(date_range)]
+
+        reservations = Booking.objects.filter(hall=hall_id)
+        reservations = [str(reservation.date) for reservation in reservations]
+
+        return render(request, 'make_a_reservation.html', {'hall': hall,
+                                                           'dates': dates,
+                                                           'reservations': reservations})
+
+    def post(self, request, hall_id):
+
+        hall = ConferenceHall.objects.get(pk=hall_id)
+        reservation_date = request.POST['reservation']
+        Booking.objects.create(date=reservation_date, hall=hall)
+
+        return redirect('reserve')
 
 
-def show_all_halls(request):
-    halls = ConferenceHall.objects.all()
-    return render(request, 'show_all_halls.html', {'halls': halls})
+class Reserve(View):
+
+    # def get(self, request, hall_id):
+    #     return render(request, 'add_a_comment.html')
+
+    def post(self, request, hall_id):
+        hall = ConferenceHall.objects.get(pk=hall_id)
+        Booking.objects.create(date=reservation_date, hall=hall)
+        return redirect('home')
